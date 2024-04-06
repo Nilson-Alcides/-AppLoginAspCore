f9ac829b5332923d49db6557b6206cd0077b66e1
Nilson Alcides <nilson_alcides@hotmail.com>
2024-04-06 05:23:57 -0300

    ClienteRepository

---
+++ AppLoginAspCore/Repository/ClienteRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace AppLoginAspCore.Repository
+{
+    public class ClienteRepository
+    {
+    }
+}
--- AppLoginAspCore/obj/Debug/net8.0/AppLoginAspCore.AssemblyInfo.cs
+++ AppLoginAspCore/obj/Debug/net8.0/AppLoginAspCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dafa65d1113dd262dd4eb2335d2737d19efce433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa75a0b0e37e7142378d2fc06e29db8131f0680")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
