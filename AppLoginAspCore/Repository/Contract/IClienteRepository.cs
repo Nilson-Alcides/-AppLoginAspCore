cfa75a0b0e37e7142378d2fc06e29db8131f0680
Nilson Alcides <nilson_alcides@hotmail.com>
2024-04-06 05:04:24 -0300

    interface IClienteRepository

---
+++ AppLoginAspCore/Repository/Contract/IClienteRepository.cs
@@ -0,0 +1,22 @@
+﻿using AppLoginCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using X.PagedList;
+
+namespace AppLoginCore.Repositories.Contracts
+{
+    public interface IClienteRepository
+    {
+        Cliente Login(string Email, string Senha);
+        
+        //CRUD
+        void Cadastrar(Cliente cliente);
+        void Atualizar(Cliente cliente);
+        void Excluir(int Id);
+        Cliente ObterCliente(int Id);
+        IEnumerable<Cliente> ObterTodosClientes();
+        //  IPagedList<Cliente> ObterTodosClientes(int? pagina, string pesquisa);
+    }
+}
--- AppLoginAspCore/obj/Debug/net8.0/AppLoginAspCore.AssemblyInfo.cs
+++ AppLoginAspCore/obj/Debug/net8.0/AppLoginAspCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8006a1ee88b6b62032c6602e5f3e8491b297a7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dafa65d1113dd262dd4eb2335d2737d19efce433")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
