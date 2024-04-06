d64bf78e75bdcabeabcb9f44488515c396ff79c6
Nilson Alcides <nilson_alcides@hotmail.com>
2024-04-06 02:59:13 -0300

    Primeiro Commit

---
+++ AppLoginAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppLoginAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
