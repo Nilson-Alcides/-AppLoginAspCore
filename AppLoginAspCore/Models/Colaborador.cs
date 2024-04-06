dafa65d1113dd262dd4eb2335d2737d19efce433
Nilson Alcides <nilson_alcides@hotmail.com>
2024-04-06 04:02:14 -0300

    Classe Colaborador

--- AppLoginAspCore/Models/Colaborador.cs
+++ AppLoginAspCore/Models/Colaborador.cs
@@ -8,9 +8,8 @@ using System.Threading.Tasks;
 
 namespace AppLoginAspCore.Models
 {
-    public class Cliente
+    public class Colaborador
     {
-        /* PK */
         [Display(Name = "Código", Description = "Código.")]
         public int Id { get; set; }
 
@@ -18,22 +17,13 @@ namespace AppLoginAspCore.Models
         [Required(ErrorMessage = "O nome completo é obrigatório.")]
         public string Nome { get; set; }
 
-        [Display(Name = "Nascimento")]
-        [Required(ErrorMessage = "A data é obrigatorio")]
-        public DateTime Nascimento { get; set; }
-
-        [Display(Name = "Sexo")]
-        [Required(ErrorMessage = "O Sexo é obrigatorio")]
-        [StringLength(1, ErrorMessage = "Deve conter 1 caracter")]
-        public string Sexo { get; set; }
-
-        [Display(Name = "CPF")]
-        [Required(ErrorMessage = "O CPF é obrigatorio")]
-        public string CPF { get; set; }
-
         [Display(Name = "Celular")]
         [Required(ErrorMessage = "O Celular é obrigatorio")]
         public string Telefone { get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "O CPF é obrigatorio")]       
+        public string CPF { get; set; }
 
         [Display(Name = "Email")]
         [EmailAddress(ErrorMessage = " O Email não é valido")]
@@ -45,9 +35,12 @@ namespace AppLoginAspCore.Models
         [Required(ErrorMessage = "O senha é obrigatorio")]
         [StringLength(10, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 10 caracteres")]
         public string Senha { get; set; }
-        
-        [Display(Name = "Situação")]
-        [Required(ErrorMessage = "O Situação é obrigatorio")]
-        public string Situacao { get; set; }
+
+        /*
+         * TIPO ColaboradorTipoConstant
+        */
+        [Display(Name = "Tipo")]
+        [Required(ErrorMessage = "O Tipo é obrigatorio")]
+        public string Tipo { get; set; } 
     }
 }
