a453e19049510c9b7b7ae17dbb67f59e09770946
Nilson Alcides <nilson_alcides@hotmail.com>
2024-04-06 23:21:58 -0300

    Cadastrar e Consultar sessão

--- AppLoginAspCore/Libraries/Sessao/Sessao.cs
+++ AppLoginAspCore/Libraries/Sessao/Sessao.cs
@@ -9,5 +9,18 @@
             _context = context;
         }
 
+        /*
+         * CRUD - Cadastrar/Atualizar/Consultar/Remover - RemoverTodos/Exist
+         */
+        //Cadastrar sessão
+        public void Cadastrar(string Key, string Valor)
+        {
+            _context.HttpContext.Session.SetString(Key, Valor);
+        }
+        //Consultar sessão
+        public string Consultar(string Key)
+        {
+            return _context.HttpContext.Session.GetString(Key);
+        }
     }
 }
