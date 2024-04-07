2a2fcef0737ded9e50f89baf71fcec3ceca5dacc
Nilson Alcides <nilson_alcides@hotmail.com>
2024-04-07 18:02:19 -0300

    Exibir login cliente no Layout

--- AppLoginAspCore/obj/Debug/net8.0/AppLoginAspCore.AssemblyInfo.cs
+++ AppLoginAspCore/obj/Debug/net8.0/AppLoginAspCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba04215eff0522554591c13cdf1ba22e5dd89831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c5c98254840bed4e00b39535b559d5790e21eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
