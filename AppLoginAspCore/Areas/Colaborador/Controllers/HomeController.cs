dae462d73120f131965e527cccbe7db1f4000a59
Nilson Alcides <nilson_alcides@hotmail.com>
2024-04-18 14:45:07 -0300

    HomeController Area do colaborador

---
+++ AppLoginAspCore/Areas/Colaborador/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppLoginAspCore.Areas.Colaborador.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AppLoginAspCore/obj/Debug/net8.0/AppLoginAspCore.AssemblyInfo.cs
+++ AppLoginAspCore/obj/Debug/net8.0/AppLoginAspCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6712e1ae884064a6f29cf3c29fdb64c808cc9284")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ed2c36aea8202f2967e9840b7a3e1f4da253cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLoginAspCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
