4b5c1832c92f7ed2759d1f8a354852c41c7c3478
Nilson Alcides <nilson_alcides@hotmail.com>
2024-04-20 03:18:29 -0300

    Filtro colaborador

--- AppLoginAspCore/Areas/Colaborador/Controllers/HomeController.cs
+++ AppLoginAspCore/Areas/Colaborador/Controllers/HomeController.cs
@@ -32,46 +32,37 @@ namespace AppLoginAspCore.Areas.Colaborador.Controllers
             Models.Colaborador colaboradorDB = _repositoryColaborador.Login(colaborador.Email, colaborador.Senha);
 
 
-            if (colaboradorDB.Email != null && colaboradorDB.Senha != null && 
-                colaboradorDB.Tipo != ColaboradorTipoConstant.Comum)
+            if (colaboradorDB.Email != null && colaboradorDB.Senha != null )
             {
                 _loginColaborador.Login(colaboradorDB);
 
-                return new RedirectResult(Url.Action(nameof(PainelGerente)));
-            }
-            if (colaboradorDB.Email != null && colaboradorDB.Senha != null && 
-                colaboradorDB.Tipo != ColaboradorTipoConstant.Gerente)
-            {
-                _loginColaborador.Login(colaboradorDB);
-
-                return new RedirectResult(Url.Action(nameof(PainelComun)));
-
-            }
+                return new RedirectResult(Url.Action(nameof(Painel)));
+            }           
             else
             {
                 ViewData["MSG_E"] = "Usuário não encontrado, verifique o e-mail e senha digitado!";
                 return View();
             }
         }
-        [ColaboradorAutorizacao]
-        public IActionResult PainelGerente()
-        {
-            ViewBag.Nome = _loginColaborador.GetColaborador().Nome;
-            ViewBag.Tipo = _loginColaborador.GetColaborador().Tipo;
-            ViewBag.Email = _loginColaborador.GetColaborador().Email;
-            //return new ContentResult() { Content = "Este é o Painel do Cliente!" };
-            return View();
-        }
-        [ColaboradorAutorizacao]
-        public IActionResult PainelComun()
-        {
-            ViewBag.Nome = _loginColaborador.GetColaborador().Nome;
-            ViewBag.Tipo = _loginColaborador.GetColaborador().Tipo;
-            ViewBag.Email = _loginColaborador.GetColaborador().Email;
-            //return new ContentResult() { Content = "Este é o Painel do Cliente!" };
-            return View();
+        //[ColaboradorAutorizacao]
+        //public IActionResult PainelGerente()
+        //{
+        //    ViewBag.Nome = _loginColaborador.GetColaborador().Nome;
+        //    ViewBag.Tipo = _loginColaborador.GetColaborador().Tipo;
+        //    ViewBag.Email = _loginColaborador.GetColaborador().Email;
+        //    return new ContentResult() { Content = "Este é o Painel do Cliente!" };
+        //    return View();
+        //}
+        //[ColaboradorAutorizacao]
+        //public IActionResult PainelComun()
+        //{
+        //    ViewBag.Nome = _loginColaborador.GetColaborador().Nome;
+        //    ViewBag.Tipo = _loginColaborador.GetColaborador().Tipo;
+        //    ViewBag.Email = _loginColaborador.GetColaborador().Email;
+        //    return new ContentResult() { Content = "Este é o Painel do Cliente!" };
+        //    return View();
 
-        }
+        //}
         [ColaboradorAutorizacao]
         public IActionResult Painel()
         {
